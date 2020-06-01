ba0c313e137481d1263ec1fdba3de71650a9362e
anshula <anshula@mit.edu>
2020-06-01 14:54:39 -0400

    semilog scale

--- assembly/comparison_plot.py
+++ assembly/comparison_plot.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.genfromtxt('data/comparison_k.csv', delimiter=',', skip_header=1)
+# PLOT WITH SEMILOG ON X AXIS 
+
+data = np.genfromtxt('data/comparison_n.csv', delimiter=',', skip_header=1)
 # print(accuracies)
 
 p_vals = []
@@ -19,14 +21,22 @@ for row in data:
 
 
 	
-plt.plot(p_vals,median_accuracies, marker='o')
-plt.title("Accuracy v.s. capped neurons")
-plt.xlabel("Value of k")
+plt.semilogx(p_vals,median_accuracies, marker='o')
+plt.title("Accuracy v.s. assembly size")
+plt.xlabel("Value of n")
+# plt.title("Accuracy v.s. edge connection probability")
+# plt.xlabel("Value of p")
+# plt.title("Accuracy v.s. capped neurons")
+# plt.xlabel("Value of k")
 plt.ylabel("Accuracy")
 plt.show()
 
-plt.plot(p_vals,results_with_over_99_percent_accuracies, marker='o')
-plt.title("Fraction of high-accuracy results v.s. capped neurons")
-plt.xlabel("Value of k")
+plt.semilogx(p_vals,results_with_over_99_percent_accuracies, marker='o')
+plt.title("Fraction of high-accuracy results v.s. assembly size")
+plt.xlabel("Value of n")
+# plt.title("Fraction of high-accuracy results v.s. edge connection probability")
+# plt.xlabel("Value of p")
+# plt.title("Fraction of high-accuracy results v.s. capped neurons")
+# plt.xlabel("Value of k")
 plt.ylabel("Fraction of results with over 99% accuracy")
 plt.show()
