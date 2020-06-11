f7877cbdb4acd6f8c3b873f7e4517c21ba54f8e6
Simon <salford@mit.edu>
2020-06-11 12:55:10 -0400

    Added list synthesis testing just to have around.

--- ec/bin/listFuncTest.py
+++ ec/bin/listFuncTest.py
@@ -1,6 +1,6 @@
+import random
 import datetime
 import os
-import random
 
 import binutil
 
@@ -8,16 +8,21 @@ from dreamcoder.dreamcoder import commandlineArguments, ecIterator
 from dreamcoder.grammar import Grammar
 from dreamcoder.program import Primitive
 from dreamcoder.task import Task
-from dreamcoder.type import arrow, tint
+from dreamcoder.type import arrow, tint, tlist
 from dreamcoder.utilities import numberOfCPUs
 
 # create primitives
 
+
+def _incrList(x): return lambda x: [x[0]]
+
 def _incr(x): return lambda x: x + 1
+
 
 primitives = [
     # Primitive(name in Ocaml, type, name in Python)
-    Primitive("incr", arrow(tint, tint), _incr)
+Primitive("incrList", arrow(tlist(tint), tlist(tint)), _incrList),
+Primitive("incr", arrow(tint, tint), _incr)
 ]
 
 # create grammar
@@ -41,6 +46,11 @@ def addN(n):
     x = random.choice(range(500))
     return {"i": x, "o": x + n}
 
+def listFunction():
+    x = random.choice(range(500))
+    return {"i": [x, 1, 2], "o": [x]}
+
+
 # Each task will consist of 3 things:
 # 1. a name
 # 2. a mapping from input to output type (e.g. `arrow(tint, tint)`)
@@ -52,22 +62,37 @@ def get_tint_task(item):
         [((ex["i"],), ex["o"]) for ex in item["examples"]],
     )
 
+def get_tlist_task(item):
+    return Task(
+        item["name"],
+        arrow(tlist(tint), tlist(tint)),
+        [((ex["i"],), ex["o"]) for ex in item["examples"]],
+    )
+
+
 # Training data
 def add1(): return addN(1)
 def add3(): return addN(3)
 def add6(): return addN(6)
 
+
 training_examples = [
     {"name": "add3", "examples": [add3() for _ in range(5000)]}
 ]
 
+list_training_examples = [
+        {"name": "addList", "examples": [listFunction() for _ in range(500)]}
+]
+
 training = [get_tint_task(item) for item in training_examples]
+training += [get_tlist_task(item) for item in list_training_examples]
 
 # Testing data
 testing_examples = [
     {"name": "add6", "examples": [add6() for _ in range(500)]},
 ]
 testing = [get_tint_task(item) for item in testing_examples]
+
 
 # iterate over wake and sleep cycles for our task
 generator = ecIterator(grammar,
