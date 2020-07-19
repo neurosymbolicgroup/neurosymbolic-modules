a3247c97d91c7b95b7051d5cc61d477fa1fe61a7
Anshula Gandhi <anshula@mit.edu>
2020-07-18 22:12:05 -0400

    arc_tempcode is still working even though using python solver

--- ec/bin/arc_tempcode.py
+++ ec/bin/arc_tempcode.py
@@ -16,9 +16,14 @@ from dreamcoder.task import Task
 from dreamcoder.type import arrow, tint
 from dreamcoder.utilities import numberOfCPUs
 
+from dreamcoder.domains.arc.arcPrimitives import tgrid, map_primitives,grid_primitives, color_primitives, ArcExample, _gridempty
+from dreamcoder.domains.arc.makeArcTasks import make_tasks_anshula
+from dreamcoder.domains.arc.main import ArcFeatureNN
+
+random.seed(0)
 # create primitives
 
-def _incr(x): return lambda x: x + 1
+def _incr(x): return x + 1
 
 primitives = [
     # Primitive(name in Ocaml, type, name in Python)
@@ -31,11 +36,21 @@ grammar = Grammar.uniform(primitives)
 
 # generic command line options
 args = commandlineArguments(
-    enumerationTimeout=10, activation='tanh',
-    iterations=10, recognitionTimeout=3600,
-    a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
-    helmholtzRatio=0.5, structurePenalty=1.,
-    CPUs=numberOfCPUs())
+    enumerationTimeout=10, 
+    activation='tanh',
+    aic=1.,
+    iterations=6, 
+    recognitionTimeout=60, 
+    # featureExtractor=ArcFeatureNN,
+    a=3, 
+    maximumFrontier=10, 
+    topK=1, 
+    pseudoCounts=30.0,
+    helmholtzRatio=0.5, 
+    structurePenalty=.001,
+    solver='python',
+    # CPUs=numberOfCPUs()
+    )
 
 
 # helper function that will 
@@ -65,6 +80,7 @@ def add9(): return addN(9)
 def add12(): return addN(12)
 
 training_examples = [
+    {"name": "add1", "examples": [add1() for _ in range(5000)]},
     {"name": "add3", "examples": [add3() for _ in range(5000)]},
     {"name": "add9", "examples": [add9() for _ in range(5000)]},
     {"name": "add6", "examples": [add6() for _ in range(5000)]}
@@ -78,6 +94,12 @@ testing_examples = [
 ]
 testing = [get_tint_task(item) for item in testing_examples]
 
+# training2, testing2 = make_tasks_anshula()
+# training  = training + training2
+# testing = testing + testing2
+
+for ex in training:
+    print("training", ex)
 # iterate over wake and sleep cycles for our task
 generator = ecIterator(grammar,
                        training,
