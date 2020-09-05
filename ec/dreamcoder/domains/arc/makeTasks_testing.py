711c1b1ba44e74577721744118fe86a5a47d3954
Anshula Gandhi <anshula@mit.edu>
2020-09-05 19:58:51 -0400

    drawlineleft
    singularity exec container.img python bin/arc_linedrawings.py

--- ec/dreamcoder/domains/arc/makeTasks_testing.py
+++ ec/dreamcoder/domains/arc/makeTasks_testing.py
@@ -53,6 +53,67 @@ def make_rotation_tasks():
             examples0
         )
 
+# ---------------------------------------------
+    # TASK that draws a line to the left
+    # ---------------------------------------------
+    
+    array0_in = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0]])
+    array0_out = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [1, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0]])
+    arc0_in = Grid(array0_in)
+    arc0_out = Grid(array0_out)
+    should_be = p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(
+                    p._draw_line_down(
+                        p._rotate_ccw(arc0_in)
+                    )(p._get(
+                        p._objects(
+                            p._rotate_ccw(arc0_in)
+                        )
+                        ) (0)
+                    )
+                )))
+    assert arc0_out == should_be, 'incorrect example created'
+
+    array1_in = np.array(
+                [[0, 0, 0, 1, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0]])
+    array1_out = np.array(
+                [[1, 1, 1, 1, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0]])
+    arc1_in = Grid(array1_in)
+    arc1_out = Grid(array1_out)
+    should_be = p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(
+                p._draw_line_down(
+                    p._rotate_ccw(arc1_in)
+                )(p._get(
+                    p._objects(
+                        p._rotate_ccw(arc1_in)
+                    )
+                    ) (0)
+                )
+            )))
+    print(arc1_out)
+    print(should_be)
+    assert arc1_out == should_be, 'incorrect example created'
+
+    examples0 = [((arc0_in,), arc0_out), ((arc1_in,), arc1_out)]
+    task_drawlinedown = Task(
+            "drawLineLeft",
+            arrow(tgrid, tgrid),
+            examples0
+        )
+
 
     # ---------------------------------------------
     # TASK that moves an object down
