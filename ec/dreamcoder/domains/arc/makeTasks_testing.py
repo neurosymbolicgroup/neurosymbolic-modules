903ac73a8c729f5d65f1eae4a97e376733178a9f
Anshula Gandhi <anshula@mit.edu>
2020-09-07 17:34:36 -0400

    8 tasks solved
    singularity exec container.img python bin/arc_linedrawings.py -t 300 -i 2

--- ec/dreamcoder/domains/arc/makeTasks_testing.py
+++ ec/dreamcoder/domains/arc/makeTasks_testing.py
@@ -151,6 +151,54 @@ def make_rotation_tasks():
             examples0
         )
 
+    # ---------------------------------------------
+    # TASK that draws a line to the right
+    # ---------------------------------------------
+    
+    array0_in = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0]])
+    array0_out = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 1]])
+    arc0_in = Grid(array0_in)
+    arc0_out = Grid(array0_out)
+    should_be = p._rotate_ccw(
+                    p._draw_line_down(
+                        p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(arc0_in)))
+                    )
+                )
+    assert arc0_out == should_be, 'incorrect example created'
+
+    array1_in = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0]])
+    array1_out = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [1, 1, 1, 1, 1],
+                 [0, 0, 0, 0, 0]])
+    arc1_in = Grid(array1_in)
+    arc1_out = Grid(array1_out)
+    should_be = p._rotate_ccw(
+                    p._draw_line_down(
+                        p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(arc1_in)))
+                    )
+                )
+    assert arc1_out == should_be, 'incorrect example created'
+
+    examples0 = [((arc0_in,), arc0_out), ((arc1_in,), arc1_out)]
+    task_drawlineright = Task(
+            "drawLineRight",
+            arrow(tgrid, tgrid),
+            examples0
+        )
 
 
     # ---------------------------------------------
@@ -295,6 +343,57 @@ def make_rotation_tasks():
             examples0
         )
 
+    # ---------------------------------------------
+    # TASK that moves an object to the right
+    # ---------------------------------------------
+    
+    array0_in = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 1, 1, 1, 0], 
+                 [0, 1, 0, 1, 0],
+                 [0, 0, 0, 1, 0]])
+
+    array0_out = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 1, 1, 1], 
+                 [0, 0, 1, 0, 1],
+                 [0, 0, 0, 0, 1]])
+    arc0_in = Grid(array0_in)
+    arc0_out = Grid(array0_out)
+    should_be = p._rotate_ccw(
+                    p._move_down(
+                        p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(arc0_in)))
+                    )
+                )
+    assert arc0_out == should_be, 'incorrect example created'
+
+    array1_in = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [1, 1, 1, 0, 0],
+                 [0, 0, 0, 0, 0]])
+    array1_out = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 1, 1, 1, 0],
+                 [0, 0, 0, 0, 0]])
+    arc1_in = Grid(array1_in)
+    arc1_out = Grid(array1_out)
+    should_be = p._rotate_ccw(
+                    p._move_down(
+                        p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(arc1_in)))
+                    )
+                )
+    assert arc1_out == should_be, 'incorrect example created'
+
+    examples0 = [((arc0_in,), arc0_out), ((arc1_in,), arc1_out)]
+    task_moveobjectright = Task(
+            "moveObjectRight",
+            arrow(tgrid, tgrid),
+            examples0
+        )
+
+
 
 
     # ---------------------------------------------
@@ -302,7 +401,8 @@ def make_rotation_tasks():
     # ---------------------------------------------
     training= [task_drawlinedown, task_moveobjectdown, 
             task_drawlineleft, task_moveobjectleft,
-            task_drawlineup, task_moveobjectup]
+            task_drawlineup, task_moveobjectup,
+            task_drawlineright, task_moveobjectright]
 
     return training
 
