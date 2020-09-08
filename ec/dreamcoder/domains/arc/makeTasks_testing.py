21ca4597959cec1b66b385cf188c490f70ba4db9
Anshula Gandhi <anshula@mit.edu>
2020-09-08 10:08:53 -0400

    creating primtiives for rotate(rotate(task(rotate))) but not generalizing the task
    singularity exec container.img python bin/arc_linedrawings.py -t 200 -i 3

--- ec/dreamcoder/domains/arc/makeTasks_testing.py
+++ ec/dreamcoder/domains/arc/makeTasks_testing.py
@@ -394,15 +394,72 @@ def make_rotation_tasks():
         )
 
 
+    # ---------------------------------------------
+    # TASK that draws both up and right arrows, and moves object to the left and down
+    # or move the object right (twice?), then you draw a line left and draw a line up
+    # ---------------------------------------------
+    
+    array0_in = np.array(
+                [[0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0]])
+
+    array0_out = np.array(
+                [[1, 0, 0, 0, 0], 
+                 [1, 0, 0, 0, 0], 
+                 [1, 0, 0, 0, 0],
+                 [1, 1, 1, 1, 1]])
+    arc0_in = Grid(array0_in)
+    arc0_out = Grid(array0_out)
+    should_be = p._rotate_ccw(
+                    p._move_down(
+                        p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(arc0_in)))
+                    )
+                )
+    # assert arc0_out == should_be, 'incorrect example created'
+
+    array1_in = np.array(
+                [[0, 0, 0, 1, 0], 
+                 [0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0]])
+    array1_out = np.array(
+                [[0, 0, 1, 0, 0], 
+                 [0, 0, 1, 0, 0], 
+                 [0, 0, 1, 1, 1],
+                 [0, 0, 0, 0, 0]])
+    arc1_in = Grid(array1_in)
+    arc1_out = Grid(array1_out)
+    should_be = p._rotate_ccw(
+                    p._move_down(
+                        p._rotate_ccw(p._rotate_ccw(p._rotate_ccw(arc1_in)))
+                    )
+                )
+    # assert arc1_out == should_be, 'incorrect example created'
+
+    examples0 = [((arc0_in,), arc0_out), ((arc1_in,), arc1_out)]
+    task_moveAndDraw2Lines = Task(
+            "moveAndDraw2Lines",
+            arrow(tgrid, tgrid),
+            examples0
+        )
+
+
+
 
 
     # ---------------------------------------------
     # PRINT
     # ---------------------------------------------
-    training= [task_drawlinedown, task_moveobjectdown, 
+    training= [
+            #task_drawlinedown, task_moveobjectdown, 
             task_drawlineleft, task_moveobjectleft,
             task_drawlineup, task_moveobjectup,
-            task_drawlineright, task_moveobjectright]
+            task_moveAndDraw2Lines
+            #task_drawlineright, task_moveobjectright
+
+            ]
 
     return training
 
