f99aa167a9e3e7b7effa934e22d347a7b5ad4d14
anshula <anshula@mit.edu>
2020-11-12 09:08:23 -0500

    zip no len error

--- ec/dreamcoder/domains/arc/neuralnets/train_sarnet.py
+++ ec/dreamcoder/domains/arc/neuralnets/train_sarnet.py
@@ -8,147 +8,123 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 from models import UNetSAR
-from sar_data_generator import SARDataGenerator
-from sarnet_config import SARConfig as conf
 
 import pickle
 import os
 import matplotlib.pyplot as plt
 
-if conf.train_regime == 'fixed':
-    if hasattr(conf, 'trainset_path') is False:
-        template = sio.loadmat(conf.template_path)['st']
-        missing_rate = 0.50
-        datagen = SARDataGenerator(template,
-                                   dim=conf.dim,
-                                   max_sparse=conf.max_sparse,
-                                   sparsity_pattern=conf.sparsity_pattern,
-                                   support_dist=conf.support_dist,
-                                   scale=conf.scale,
-                                   num_clusters=conf.num_clusters,
-                                   normalize_signals=conf.normalize_signals)
-        signals, measurements = datagen.generate_and_save_dataset(conf.trainset_size, missing_rate=missing_rate)
-        signals, measurements = torch.Tensor(signals), torch.Tensor(measurements)
-    else:
-        with open(conf.trainset_path,'rb') as f:
-            dataset = pickle.load(f)
-            signals, measurements = torch.Tensor(dataset['signals']), torch.Tensor(dataset['measurements'])
+config = {
+    "batch_size": 2,
+    "learning_rate": .1,
+    "max_epochs": 10
+}
 
-    trainloader = torch.utils.data.DataLoader(zip(signals, measurements), batch_size=conf.batch_size)
-    print('Loaded Training Dataset')
+signals = np.random.rand(1000,5) # 5 training samples in 1000-dim space
+measurements = np.random.randint(2, size=5) # 5 training examples classified as a 0 or 1
+signals, measurements = torch.Tensor(signals), torch.Tensor(measurements)
 
-elif conf.train_regime == 'online':
-    template = sio.loadmat(conf.template_path)['st']
-    missing_rate = 0.50
-    datagen = SARDataGenerator(template,
-                               dim=conf.dim,
-                               max_sparse=conf.max_sparse,
-                               sparsity_pattern=conf.sparsity_pattern,
-                               support_dist=conf.support_dist,
-                               scale=conf.scale,
-                               num_clusters=conf.num_clusters,
-                               normalize_signals=conf.normalize_signals)
-    print('Created Training Simulator')
+trainloader = torch.utils.data.DataLoader(zip(signals, measurements), batch_size=config["batch_size"])
+print('Loaded Training Dataset')
 
-if conf.criterion == 'l1':
-    criterion = nn.L1Loss()
-elif conf.criterion == 'mse':
-    criterion = nn.MSELoss()
-elif conf.criterion == 'smoothl1':
-    criterion = nn.SmoothL1Loss()
-else:
-    raise Exception('Invalid Criterion, defaulting to L1')
-    criterion = nn.L1Loss()
+# if conf.criterion == 'l1':
+criterion = nn.L1Loss()
+# elif conf.criterion == 'mse':
+#     criterion = nn.MSELoss()
+# elif conf.criterion == 'smoothl1':
+#     criterion = nn.SmoothL1Loss()
+# else:
+#    raise Exception('Invalid Criterion, defaulting to L1')
 
 net = UNetSAR()
-if torch.cuda.is_available() and conf.use_gpu:
+if torch.cuda.is_available():
     net = net.cuda()
     criterion = criterion.cuda()
 
 print('Loaded Network')
 
 losses = []
-optimizer = optim.Adam(net.parameters(), lr=conf.learning_rate)
-if conf.lr_schedule:
-    scheduler = optim.lr_scheduler.StepLR(optimizer,
-                                          conf.lr_schedule_interval,
-                                          gamma=conf.lr_schedule_gamma)
+optimizer = optim.Adam(net.parameters(), lr=config["learning_rate"])
 
-if conf.train_regime == 'fixed':
-    for epoch in range(conf.max_epochs):
-        running_loss = 0.0
+# if conf.lr_schedule:
+# scheduler = optim.lr_scheduler.StepLR(optimizer,
+#                                           conf.lr_schedule_interval,
+#                                           gamma=conf.lr_schedule_gamma)
 
-        for i,data in enumerate(trainloader,0):
-            labels, inputs = data
-            if torch.cuda.is_available() and conf.use_gpu:
-                labels, inputs = Variable(labels.cuda()), Variable(inputs.cuda())
+# if conf.train_regime == 'fixed':
+for epoch in range(config["max_epochs"]):
+    running_loss = 0.0
 
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            losses.append(loss)
-            running_loss += np.float(loss.data.cpu().numpy())
-            if conf.verbose and (i+1) % conf.print_every == 0:
-                print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss/conf.print_every))
-                running_loss = 0.0
+    for i,data in enumerate(trainloader,0):
+        labels, inputs = data
+        if torch.cuda.is_available():
+            labels, inputs = Variable(labels.cuda()), Variable(inputs.cuda())
 
-            if conf.verbose and i==0 and epoch==0:
-                print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss))
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss)
+        running_loss += np.float(loss.data.cpu().numpy())
+        if conf.verbose and (i+1) % conf.print_every == 0:
+            print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss/conf.print_every))
+            running_loss = 0.0
 
-        if conf.lr_schedule:
-            scheduler.step()
-        if (epoch+1) % conf.save_every == 0 or epoch+1==conf.max_epochs:
-            if conf.save_training_plot:
-                fig = plt.figure(figsize=(16, 9));
-                plt.title("Training loss")
-                plt.plot(losses)
-                plt.xlabel("Iteration")
-                plt.ylabel(conf.criterion)
-                plt.tight_layout()
-                plt.savefig(os.path.join(conf.figure_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.png'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
+        if conf.verbose and i==0 and epoch==0:
+            print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss))
 
-            torch.save(net.cpu(),os.path.join(conf.save_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.pkl'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
-            if torch.cuda.is_available() and conf.use_gpu:
-                net = net.cuda()
+    if conf.lr_schedule:
+        scheduler.step()
+    if (epoch+1) % conf.save_every == 0 or epoch+1==conf.max_epochs:
+        if conf.save_training_plot:
+            fig = plt.figure(figsize=(16, 9));
+            plt.title("Training loss")
+            plt.plot(losses)
+            plt.xlabel("Iteration")
+            plt.ylabel(conf.criterion)
+            plt.tight_layout()
+            plt.savefig(os.path.join(conf.figure_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.png'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
 
-elif conf.train_regime == 'online':
-    for epoch in range(conf.max_epochs):
-        running_loss = 0.0
+        torch.save(net.cpu(),os.path.join(conf.save_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.pkl'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
+        if torch.cuda.is_available() and conf.use_gpu:
+            net = net.cuda()
 
-        for i in range(conf.iters_per_epoch):
-            labels, inputs = datagen.generate_batch(conf.batch_size, missing_rate=missing_rate)
-            labels, inputs = torch.Tensor(labels). torch.Tensor(inputs)
-            if torch.cuda.is_available() and conf.use_gpu:
-                labels, inputs = Variable(labels.cuda()), Variable(inputs.cuda())
+# elif conf.train_regime == 'online':
+#     for epoch in range(conf.max_epochs):
+#         running_loss = 0.0
 
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            losses.append(loss)
-            running_loss += np.float(loss.data.cpu().numpy())
-            if conf.verbose and (i+1) % conf.print_every == 0:
-                print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss/conf.print_every))
-                running_loss = 0.0
+#         for i in range(conf.iters_per_epoch):
+#             labels, inputs = datagen.generate_batch(conf.batch_size, missing_rate=missing_rate)
+#             labels, inputs = torch.Tensor(labels). torch.Tensor(inputs)
+#             if torch.cuda.is_available() and conf.use_gpu:
+#                 labels, inputs = Variable(labels.cuda()), Variable(inputs.cuda())
 
-            if conf.verbose and i==0 and epoch==0:
-                print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss))
+#             optimizer.zero_grad()
+#             outputs = net(inputs)
+#             loss = criterion(outputs, labels)
+#             loss.backward()
+#             optimizer.step()
+#             losses.append(loss)
+#             running_loss += np.float(loss.data.cpu().numpy())
+#             if conf.verbose and (i+1) % conf.print_every == 0:
+#                 print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss/conf.print_every))
+#                 running_loss = 0.0
 
-        if conf.lr_schedule:
-            scheduler.step()
-        if (epoch+1) % conf.save_every == 0 or epoch+1==conf.max_epochs:
-            if conf.save_training_plot:
-                fig = plt.figure(figsize=(16, 9));
-                plt.title("Training loss")
-                plt.plot(losses)
-                plt.xlabel("Iteration")
-                plt.ylabel(conf.criterion)
-                plt.tight_layout()
-                plt.savefig(os.path.join(conf.figure_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.png'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
+#             if conf.verbose and i==0 and epoch==0:
+#                 print('[epoch: %d, iter: %d] loss: %.5f' % (epoch+1, i+1, running_loss))
 
-            torch.save(net.cpu(),os.path.join(conf.save_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.pkl'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
-            if torch.cuda.is_available() and conf.use_gpu:
-                net = net.cuda()
+#         if conf.lr_schedule:
+#             scheduler.step()
+#         if (epoch+1) % conf.save_every == 0 or epoch+1==conf.max_epochs:
+#             if conf.save_training_plot:
+#                 fig = plt.figure(figsize=(16, 9));
+#                 plt.title("Training loss")
+#                 plt.plot(losses)
+#                 plt.xlabel("Iteration")
+#                 plt.ylabel(conf.criterion)
+#                 plt.tight_layout()
+#                 plt.savefig(os.path.join(conf.figure_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.png'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
+
+#             torch.save(net.cpu(),os.path.join(conf.save_dir, 'UNet_%s_%s_epoch%d_maxsparse%d.pkl'%(conf.train_regime, conf.criterion, epoch+1, conf.max_sparse)))
+#             if torch.cuda.is_available() and conf.use_gpu:
+#                 net = net.cuda()
