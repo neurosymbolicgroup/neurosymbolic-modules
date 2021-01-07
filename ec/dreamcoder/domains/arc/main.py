5af4c19ceddc07cbe1724e2fe2b17c7f62ee9eb5
twang6 <twang6@mit.edu>
2021-01-06 20:19:04 -0800

    Make ArcNet random

--- ec/dreamcoder/domains/arc/main.py
+++ ec/dreamcoder/domains/arc/main.py
@@ -100,9 +100,9 @@ class ArcNet(nn.Module):
         x = torch.sum(x, 0)
 
         # test if this is actually helping.
-        # return torch.rand(x.shape)
-
-        return x
+        # TODO: test all zeros
+        return torch.rand(x.shape)
+        # return x
 
     def make_features(self, examples):
         # zero pad, concatenate, one-hot encode
