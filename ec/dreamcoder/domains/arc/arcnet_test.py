dfdd1f3ab5245732d6a816fdf336d94beb9770dc
Simon <salford@mit.edu>
2021-01-14 21:31:52 -0500

    testing arcnet

---
+++ ec/dreamcoder/domains/arc/arcnet_test.py
@@ -0,0 +1,194 @@
+from dreamcoder.domains.arc.arcInput import num_to_id, load_task
+from dreamcoder.domains.arc.arcPrimitives import Grid, Input
+from dreamcoder.domains.arc.arcPrimitives import primitive_dict as p
+from dreamcoder.domains.arc.main import ArcNet
+from dreamcoder.task import Task
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import math
+import random
+import time
+
+
+def get_grids():
+    grids = []
+    for i in range(400):
+        task_id = num_to_id(i)
+
+        task_dict = load_task(task_id, task_path="data/ARC/data/training/")
+        for x in task_dict["train"] + task_dict["test"]:
+            grids.append(x["input"])
+            grids.append(x["output"])
+
+    return grids
+
+def get_operators():
+    names = ['rotate_cw', 'rotate_ccw', 'y_mirror', 'x_mirror', 'top_half', 'right_half', 'bottom_half', 'left_half', 'shell', 'hollow']
+
+    # the lambda function for each of them.
+    operators = [p[name].value for name in names]
+
+    d = dict(zip(names, operators))
+    d['identity'] = lambda x: x
+
+    return d
+
+def export_data(samples, path):
+    # samples is (grid, grid, name)
+    def to_str(grid):
+        return str(grid.shape) + ', ' + ' '.join(map(str, list(grid.flatten())))
+
+    def line_of_example(example):
+        inp, outp, op = example
+        return 'IN: {}, OUT: {}, OP: {}'.format(to_str(inp.grid),
+                                                to_str(outp.grid),
+                                                op)
+
+    lines = '\n'.join(line_of_example(s) for s in samples)
+    with open(path, 'w+') as f:
+        f.write(lines)
+
+
+def generate_dataset():
+    grids = get_grids()
+    grids = [g for g in grids if min(g.shape) > 2]
+    grids = [Grid(g) for g in grids]
+    print('{} grids'.format(len(grids)))
+
+    operators = get_operators()
+    print('{} operators'.format(len(operators)))
+
+
+    num_unique = 0
+    num_total = len(operators)*len(grids)
+    print('num_total: {}'.format(num_total))
+    samples = [(grid, f(grid), name) for grid in grids for name, f in
+            operators.items()]
+    # for grid in grids:
+    #     outputs = []
+    #     for f in operators.values():
+    #         match = np.any([np.all(o == f(grid)) for o in outputs])
+    #         if not match:
+    #             num_unique += 1
+    #             outputs.append(f(grid))
+
+    # print('num_unique: {}'.format(num_unique))
+
+    # export_data(samples, 'arcnet_data.txt')
+    data = import_data('arcnet_data.txt')
+    export_data(data, 'arcnet_data2.txt')
+
+
+
+def import_data(path):
+    def parse_example(line):
+        def parse_grid(grid_txt):
+            i1 = grid_txt.index('(')
+            assert i1 == 0
+            i2 = grid_txt.index(')')
+            shape = grid_txt[i1 + 1:i2]
+            shape = shape.split(', ')
+            w, h = list(map(int, shape))
+
+            rest = grid_txt[grid_txt.index(')') + 3:]
+            one_d = np.fromstring(rest, dtype=int, sep=' ')
+            out = one_d.reshape(w, h)
+            return out
+
+        i1 = line.index('IN: ')
+        i2 = line.index('OUT: ')
+        i3 = line.index('OP: ')
+        grid1 = line[i1 + 4:i2 - 2]
+        grid2 = line[i2 + 5:i3 - 2]
+        op = line[i3 + 4:-1]
+        grid1 = parse_grid(grid1)
+        grid2 = parse_grid(grid2)
+        examples = [((Input(grid1, []),), Grid(grid2))]
+
+        return examples, op
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        return [parse_example(l) for l in lines]
+
+
+class FullNet(nn.Module):
+    """Puts a FC layer onto ArcNet to match the output dim."""
+    def __init__(self, out_dim):
+        super().__init__()
+        self.arc_net = ArcNet(tasks=[])
+        self.fc = nn.Linear(self.arc_net.outputDimensionality, out_dim)
+
+    def forward(self, examples):
+        return self.fc(self.arc_net(self.arc_net.make_features(examples)))
+
+
+def train():
+    torch.set_num_threads(10)
+    data = import_data('arcnet_data.txt')
+    ops = sorted(list(set([d[1] for d in data])))
+    op_dict = dict(zip(ops, range(len(ops))))
+
+    net = FullNet(len(op_dict))
+    optimizer = optim.Adam(net.parameters(), lr=0.0001)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    batch_size = 16
+    epochs = 10000
+
+    def make_batches(data, batch_size):
+        return [data[batch_size * i: min((i + 1) * batch_size, len(data))]
+                for i in range(math.ceil(len(data) / batch_size))]
+
+    print('Starting training...')
+    for epoch in range(1, epochs+1):
+        random.shuffle(data)
+        batches = make_batches(data, batch_size)
+        start = time.time()
+
+        total_loss = 0
+        total_correct = 0
+        for i, batch in enumerate(batches):
+            # batch is list of (in_grid, out_grid, op_str) tuples
+            examples, targets = zip(*batch)
+            targets = [op_dict[t] for t in targets]
+            # print('targets: {}'.format(targets))
+            targets_tensor = torch.tensor(targets)
+
+            optimizer.zero_grad()
+
+            out = [net(e) for e in examples]
+            out = [t.unsqueeze(0) for t in out]
+            out = torch.cat(out)
+            predictions = torch.argmax(out, dim=1)
+            # print('predictions: {}'.format(predictions))
+
+            loss = criterion(out, targets_tensor)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.sum().item()
+
+            num_correct = sum(t == p for t, p in zip(targets,
+                              predictions))
+            # print('num_correct: {}'.format(num_correct))
+
+            total_correct += num_correct
+
+        accuracy = 100*total_correct / len(data)
+        duration = time.time() - start
+        m = math.floor(duration / 60)
+        s = duration - m * 60
+        duration = f'{m}m {int(s)}s'
+
+        print(f'Epoch {epoch} completed ({duration}) accuracy: {accuracy:.2f} loss: {loss:.2f}')
+
+
+    print('Finished Training')
+
+
+
