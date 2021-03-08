cd8007dc3f5c14f3f0c032802ecd641b79c94511
Simon <salford@mit.edu>
2021-03-08 03:33:29 -0500

    test search start

---
+++ bidir-synth/rl/test_search.py
@@ -0,0 +1,43 @@
+from typing import Dict, Sequence
+import torch.nn as nn
+import signal
+
+def policy_rollouts(model: nn.Module, ops: Sequence[Op], tasks: Sequence[Task], timeout: int) -> Dict:
+    """
+    Timeout is in seconds.
+    """
+    NUM_ACTIONS = 100
+    # divide logits by the temperature before sampling. Higher means more
+    # random.
+    # between (0, inf]. 0 means argmax, 1 means normal probs, inf means random.
+    TEMPERATURE = 1
+    # each time we do a rollout, just choose a task randomly from those as yet
+    # unsolved.
+    signal.alarm(timeout)
+    try:
+        unsolved_tasks = list(tasks)
+        while True:
+            task = random.choice(unsolved_tasks)
+            env = SynthEnv(ops, task, max_actions=NUM_ACTIONS)
+            obs = env.reset()
+
+            while not env.done():
+                pred = policy_net(obs.psg)
+                act = SynthEnvAction(pred.op_idx, pred.arg_idxs)
+                obs, rew, done, _ = env.step(act)
+
+    def __init__(
+        self,
+        ops: Sequence[Op],
+        task: Task = None,
+        task_sampler: Callable[[], Task] = None,
+        max_actions=100,
+        solve_reward=100,
+        synth_error_penalty=-1,
+        timeout_penalty=0,
+    ):
+
+
+    except Exception as e:
+        print(type(e))
+        assert False
